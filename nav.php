486fddeb9a715b3320ba28fde1939f9113fceea4
Jonah Cote <71891032+jonahcote@users.noreply.github.com>
2020-11-16 10:51:43 -0500

    Added links to all pages

--- nav.php
+++ nav.php
@@ -6,5 +6,29 @@
         print('activePage');
     }
     ?>" href="index.php">Home</a>
+    
+    <a class ="<?php
+    if ($path_parts['filename'] == 'gallery') {
+        print('activePage');
+    }
+    ?>" href="gallery.php">Gallery</a>
+    
+    <a class ="<?php
+    if ($path_parts['filename'] == 'about') {
+        print('activePage');
+    }
+    ?>" href="about.php">About</a>
+    
+    <a class ="<?php
+    if ($path_parts['filename'] == 'contact') {
+        print('activePage');
+    }
+    ?>" href="contact.php">Contact</a>
+    
+    <a class ="<?php
+    if ($path_parts['filename'] == 'join') {
+        print('activePage');
+    }
+    ?>" href="join.php">Join</a>
 
 </nav>
