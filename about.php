4bfd0246fdf5563d6afe0a5f2641d69d5e54c5e9
Jonah Cote <71891032+jonahcote@users.noreply.github.com>
2020-11-16 11:14:21 -0500

    Update about.php
    Added filler text and images to check layout

--- about.php
+++ about.php
@@ -1,8 +1,25 @@
 <?php include "top.php"; ?>
 
 <main>
-    <section class="ADDNAME">
-        
+    <section>
+        About the Club</p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper varius rutrum. Mauris ultricies augue felis, non fringilla felis tincidunt a. Suspendisse ornare turpis tortor, eu interdum est vestibulum vitae. Ut viverra magna eu massa ultrices, non euismod ligula tempus. Sed erat elit, facilisis non elit sit amet, iaculis blandit ante. Maecenas quis leo sed ipsum varius condimentum. Mauris vitae libero odio. Suspendisse quis porta arcu. Etiam vel laoreet dolor, ut volutpat dolor.</p>
+    </section>
+    <section>
+        <img src="camera.webp" alt="Camera on black background">
+    </section>
+    <section>
+        <p>About the founders:</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper varius rutrum. Mauris ultricies augue felis, non fringilla felis tincidunt a. Suspendisse ornare turpis tortor, eu interdum est vestibulum vitae. Ut viverra magna eu massa ultrices, non euismod ligula tempus. Sed erat elit, facilisis non elit sit amet, iaculis blandit ante. Maecenas quis leo sed ipsum varius condimentum. Mauris vitae libero odio. Suspendisse quis porta arcu. Etiam vel laoreet dolor, ut volutpat dolor.</p>
+    </section>
+    <section>
+        Founders group picture
+    </section>
+    <section>
+        CONTACT 
+    </section>
+    <section>
+        JOIN
     </section>
 </main>
 
