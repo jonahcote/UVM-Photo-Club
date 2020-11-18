a43fe8277c6e651df9dc6b5ca71202717a39b4e9
MurphyPeisel <73511421+MurphyPeisel@users.noreply.github.com>
2020-11-18 11:38:21 -0500

    Update about.php

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper varius rutrum. Mauris ultricies augue felis, non fringilla felis tincidunt a. Suspendisse ornare turpis tortor, eu interdum est vestibulum vitae. Ut viverra magna eu massa ultrices, non euismod ligula tempus. Sed erat elit, facilisis non elit sit amet, iaculis blandit ante. Maecenas quis leo sed ipsum varius condimentum. Mauris vitae libero odio. Suspendisse quis porta arcu. Etiam vel laoreet dolor, ut volutpat dolor.</p>
     </section>
     <section>
-        <img src="camera.webp" alt="Camera on black background">
+        <img src="image/camera.webp" alt="Camera on black background">
     </section>
     <section>
         <p>About the founders:</p>
