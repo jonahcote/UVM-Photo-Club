d995e537348647444556e9029838158c6f7f0b5e
MurphyPeisel <73511421+MurphyPeisel@users.noreply.github.com>
2020-11-23 11:42:24 -0500

    Update about.php

--- about.php
+++ about.php
@@ -24,7 +24,7 @@
             <h3>Murphy Peisel, Grandmaster</h3>
             <p></p>
             <h3>Jonah Cote, COO</h3>
-            <p>Born in the rugged North East Kingdom of Vermont, Jonah knows absolutely nothing about photography.  He hopes to learn about cameras beyond the one in is phone from Murphy the Master.</p>
+            <p>Born in the rugged North East Kingdom of Vermont, Jonah knows absolutely nothing about photography.  He hopes to learn about cameras beyond the one in his phone from Murphy the Master.</p>
             <p>Jonah handles new member recruitment, graphics, and outreach.</p>
             <h3>Brandon Schoenfeld, Intern</h3>
             <p>Brandon is, by far, the most experienced member of the team.  He's run a photography club before.  As such, he's the lowest ranking member of the club and mainly gets the other two coffee.</p>
