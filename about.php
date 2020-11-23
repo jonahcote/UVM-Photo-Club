8298dc47d1e487fa5124537c2c03a8d53a3c61c0
Jonah Cote <71891032+jonahcote@users.noreply.github.com>
2020-11-23 11:40:23 -0500

    Update about.php

--- about.php
+++ about.php
@@ -4,7 +4,15 @@
     <article>
         <section>
             <h2>About the Club</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper varius rutrum. Mauris ultricies augue felis, non fringilla felis tincidunt a. Suspendisse ornare turpis tortor, eu interdum est vestibulum vitae. Ut viverra magna eu massa ultrices, non euismod ligula tempus. Sed erat elit, facilisis non elit sit amet, iaculis blandit ante. Maecenas quis leo sed ipsum varius condimentum. Mauris vitae libero odio. Suspendisse quis porta arcu. Etiam vel laoreet dolor, ut volutpat dolor.</p>
+            <h3>Mission Statement</h3>
+            <p>The UVM Photography Club operates on three core missions:</p>
+            <ol>
+                <li>Educate people on the art and science of photography</li>
+                <li>Spread the love of beautiful pictures</li>
+                <li>Capture the beauty of UVM</li>
+            </ol>
+            <h3>History</h3>
+            <p>As the club doesn't exist yet, there is no history.</p>
         </section>
         <section>
             <img src="image/camera.webp" alt="Camera on black background">
@@ -13,7 +21,13 @@
     <article>
         <section>
             <h2>About the founders:</h2>
-            <h3></h3>
+            <h3>Murphy Peisel, Grandmaster</h3>
+            <p></p>
+            <h3>Jonah Cote, COO</h3>
+            <p>Born in the rugged North East Kingdom of Vermont, Jonah knows absolutely nothing about photography.  He hopes to learn about cameras beyond the one in is phone from Murphy the Master.</p>
+            <p>Jonah handles new member recruitment, graphics, and outreach.</p>
+            <h3>Brandon Schoenfeld, Intern</h3>
+            <p>Brandon is, by far, the most experienced member of the team.  He's run a photography club before.  As such, he's the lowest ranking member of the club and mainly gets the other two coffee.</p>
         </section>
         <section>
             <p>Founders group picture</p>
