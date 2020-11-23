a3b25368d562e73a0b6951b71bd9795224099484
Jonah Cote <71891032+jonahcote@users.noreply.github.com>
2020-11-23 11:32:48 -0500

    Update about.php

--- about.php
+++ about.php
@@ -1,26 +1,24 @@
 <?php include "top.php"; ?>
 
 <main>
-    <section>
-        <p>About the Club</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper varius rutrum. Mauris ultricies augue felis, non fringilla felis tincidunt a. Suspendisse ornare turpis tortor, eu interdum est vestibulum vitae. Ut viverra magna eu massa ultrices, non euismod ligula tempus. Sed erat elit, facilisis non elit sit amet, iaculis blandit ante. Maecenas quis leo sed ipsum varius condimentum. Mauris vitae libero odio. Suspendisse quis porta arcu. Etiam vel laoreet dolor, ut volutpat dolor.</p>
-    </section>
-    <section>
-        <img src="image/camera.webp" alt="Camera on black background">
-    </section>
-    <section>
-        <p>About the founders:</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper varius rutrum. Mauris ultricies augue felis, non fringilla felis tincidunt a. Suspendisse ornare turpis tortor, eu interdum est vestibulum vitae. Ut viverra magna eu massa ultrices, non euismod ligula tempus. Sed erat elit, facilisis non elit sit amet, iaculis blandit ante. Maecenas quis leo sed ipsum varius condimentum. Mauris vitae libero odio. Suspendisse quis porta arcu. Etiam vel laoreet dolor, ut volutpat dolor.</p>
-    </section>
-    <section>
-        <p>Founders group picture</p>
-    </section>
-    <section>
-        <p>CONTACT</p>
-    </section>
-    <section>
-        <p>JOIN</p>
-    </section>
+    <article>
+        <section>
+            <h2>About the Club</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper varius rutrum. Mauris ultricies augue felis, non fringilla felis tincidunt a. Suspendisse ornare turpis tortor, eu interdum est vestibulum vitae. Ut viverra magna eu massa ultrices, non euismod ligula tempus. Sed erat elit, facilisis non elit sit amet, iaculis blandit ante. Maecenas quis leo sed ipsum varius condimentum. Mauris vitae libero odio. Suspendisse quis porta arcu. Etiam vel laoreet dolor, ut volutpat dolor.</p>
+        </section>
+        <section>
+            <img src="image/camera.webp" alt="Camera on black background">
+        </section>
+    </article>
+    <article>
+        <section>
+            <h2>About the founders:</h2>
+            <h3></h3>
+        </section>
+        <section>
+            <p>Founders group picture</p>
+        </section>
+    </article>
 </main>
 
 <?php include "footer.php"; ?>
