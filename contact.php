573f5129b6e9cc0e4872a5aed4b1c1b594181183
Murphy Peisel <73511421+MurphyPeisel@users.noreply.github.com>
2020-12-02 18:10:45 -0500

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,42 +1,58 @@
-  
-<?php include "top.php";?>
-    <title>Add Map</title>
-    <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDAoeZJ6Yki5l1ILAfASFH5vLmGBbQQGPc&callback=initMap&libraries=&v=weekly"
-      defer
-    ></script>
-    <style type="text/css">
-      /* Set the size of the div element that contains the map */
-      #map {
+<?php include "top.php"; ?>
+<title>Add Map</title>
+<script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDAoeZJ6Yki5l1ILAfASFH5vLmGBbQQGPc&callback=initMap&libraries=&v=weekly"
+    defer
+></script>
+<style type="text/css">
+    /* Set the size of the div element that contains the map */
+    #map {
         height: 400px;
         /* The height is 400 pixels */
         width: 100%;
         /* The width is the width of the web page */
-      }
-    </style>
-    <script>
-      // Initialize and add the map
-      function initMap() {
+    }
+</style>
+<script>
+    // Initialize and add the map
+    function initMap() {
         // The location of Uluru
-        const uluru = { lat: -25.344, lng: 131.036 };
+        const uluru = {lat: -25.344, lng: 131.036};
         // The map, centered at Uluru
         const map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 4,
-          center: uluru,
+            zoom: 4,
+            center: uluru,
         });
         // The marker, positioned at Uluru
         const marker = new google.maps.Marker({
-          position: uluru,
-          map: map,
+            position: uluru,
+            map: map,
         });
-      }
-    </script>
+    }
+</script>
 
-    <h3>My Google Maps Demo</h3>
-    <!--The div element for the map -->
-    <div id="map"></div>
+<h3>My Google Maps Demo</h3>
+<!--The div element for the map -->
+<div id="map"></div>
 
+<table>
+    <caption>People To Contact</caption>
+<?php
+$sql = 'SELECT fldName, fldEmail, fldPosition FROM tblPhotoMemberInfo';
 
+$statement = $pdo->prepare($sql);
+$statement->execute();
+
+$records = $statement->fetchAll();
+foreach ($records as $record) {
+    print '<tr>';
+    print '<td>' . $record['fldName'] . '</td>';
+    print '<td>' . $record['fldEmail'] . '</td>';
+    print '<td>' . $record['fldPosition'] . '</td>';
+    print '</tr>' . PHP_EOL;
+}
+?>
+</table>
 </main>
 
 <?php include "footer.php"; ?>
--- gallery.php
+++ gallery.php
@@ -4,20 +4,20 @@
     <section class="ADDNAME">
         <section class="galleryRow">
             <section class="galleryColumn">
-                <img src="images/baseball.jpg">
-                <img src="images/burlington.jpg">
-                <img src="images/flags.jpg">
+                <a href="images/baseball.jpg"><img src="images/baseball.jpg"></a>
+                <a href="images/burlington.jpg"><img src="images/burlington.jpg"></a>
+                <a href="images/flags.jpg"><img src="images/flags.jpg"></a>
             </section>
             <section class="galleryColumn">
-                <img src="images/groton.jpg">
-                <img src="images/lowell.jpg">
-                <img src="images/roofs.jpg">
+                <a href="images/groton.jpg"><img src="images/groton.jpg"></a>
+                <a href="images/lowell.jpg"><img src="images/lowell.jpg"></a>
+                <a href="images/roofs.jpg"><img src="images/roofs.jpg"></a>
             </section>
             <section class="galleryColumn">
-                <img src="images/santa.jpg">
-                <img src="images/sky.jpg">
-                <img src="images/tree.jpg">
-                <img src="images/trees.jpg">
+                <a href="images/santa.jpg"><img src="images/santa.jpg"></a>
+                <a href="images/sky.jpg"><img src="images/sky.jpg"></a>
+                <a href="images/tree.jpg"><img src="images/tree.jpg"></a>
+                <a href="images/trees.jpg"><img src="images/trees.jpg"></a>
             </section>
         </section>
     </section>
