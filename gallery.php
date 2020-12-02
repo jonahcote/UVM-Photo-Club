427c0a26b1036a98056bf10b3521039e36090255
Jonah Cote <71891032+jonahcote@users.noreply.github.com>
2020-12-02 11:13:38 -0500

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -2,7 +2,24 @@
 
 <main>
     <section class="ADDNAME">
-        
+        <section class="galleryRow">
+            <section class="galleryColumn">
+                <img src="images/baseball.jpg">
+                <img src="images/burlington.jpg">
+                <img src="images/flags">
+            </section>
+            <section class="column">
+                <img src="images/groton.jpg">
+                <img src="images/lowell.jpg">
+                <img src="images/roofs.jpg">
+            </section>
+            <section class="column">
+                <img src="images/santa.jpg">
+                <img src="images/sky.jpg">
+                <img src="images/tree.jpg">
+                <img src="images/trees.jpg">
+            </section>
+        </section>
     </section>
 </main>
 
