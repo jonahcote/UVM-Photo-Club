6fc3adb54b2047134dc3fcef519855b787ce420a
Murphy Peisel <73511421+MurphyPeisel@users.noreply.github.com>
2020-12-03 16:56:41 -0500

    Add files via upload

--- gallery.php
+++ gallery.php
@@ -4,7 +4,13 @@
     <section class="ADDNAME">
         <section class="galleryRow">
             <section class="galleryColumn">
-                <a href="images/baseball.jpg"><img src="images/baseball.jpg"></a>
+                <a href="images/baseball.jpg"><img src="images/baseball-med.jpg"
+                                                   sizes="(min-width: 600px) 25vw, 
+                                                   (min-width: 400px) 50vw, 
+                                                   100vw"
+                                                   srcset="images/baseball-small.jpg 200w, 
+                                                   images/baseball-med.jpg 400w,
+                                                   images/baseball-large.jpg 800w"></a>
                 <a href="images/burlington.jpg"><img src="images/burlington.jpg"></a>
                 <a href="images/flags.jpg"><img src="images/flags.jpg"></a>
             </section>
